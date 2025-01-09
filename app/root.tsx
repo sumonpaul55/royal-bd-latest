b3d5444bb5821d0c328cc6afe38633cdf8589a7f
sumonpaul55 <sumpaul100@gmail.com>
2025-01-10 00:15:36 +0600

    dashboard layout completed with responsive ness

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,6 @@
 import {
   isRouteErrorResponse,
+  Link,
   Links,
   Meta,
   Outlet,
@@ -9,6 +10,7 @@ import {
 
 import type { Route } from "./+types/root";
 import stylesheet from "./app.css?url";
+import { useState } from "react";
 
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -43,21 +45,66 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
   return <>
-    <div className="flex">
-      <div className="md:w-[350px] h-[100vh] text-white bg-slate-700 p-2 md:p-5 overflow-y-auto">
-        <h1>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis, tenetur.</h1>
+    <div className="flex min-h-screen">
+      {/* Sidebar */}
+      <div
+        className={`fixed md:static md:w-1/5 bg-gray-800 text-white transform ${isSidebarOpen ? "translate-x-0" : "-translate-x-full"
+          } transition-transform duration-300 ease-in-out z-50 md:translate-x-0 h-screen border-r`}>
+        <div className="p-4">
+          <h2 className="text-2xl font-bold border-b pb-3 border-slate-600">Dashboard</h2>
+          <ul className="mt-6 space-y-2">
+            <li>
+              <Link to="/" className="block py-2 px-4 rounded hover:bg-gray-700 border-b border-slate-500">
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link to="/about-us" className="block py-2 px-4 rounded hover:bg-gray-700 border-b border-slate-500">
+                About
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
-      <div className="flex-1 bg-slate-100 overflow-y-auto">
-        <div>
-          dashboard topbar
+
+      {/* Main Content */}
+      <div className="flex-1 md:ml-1/4 bg-gray-100">
+        {/* Navbar for mobile */}
+        <div className="bg-gray-800 text-white p-4 flex md:hidden">
+
+          <div className="flex gap-2">
+            <h1 className="text-lg font-bold">Dashboard</h1>
+            <button
+              onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+              className={`mr-4 focus:outline-none z-50 ${isSidebarOpen ? "ml-10" : ""}`}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth={2}
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
+              </svg>
+            </button>
+          </div>
         </div>
-        <div className="h-screen">
-          <Outlet />
+        <div className="">
+          <div className="bg-gray-800 py-3 text-white px-3 md:px-6">
+            <h1 className="text-xl font-bold">Welcome to the Dashboard</h1>
+          </div>
+          <div className="p-3 md:p-5">
+            <Outlet />
+          </div>
         </div>
-        <footer>
-          Footer
-        </footer>
       </div>
     </div>
   </>;
@@ -75,7 +122,7 @@ export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
         ? "The requested page could not be found."
         : error.statusText || details;
   } else if (import.meta.env.DEV && error && error instanceof Error) {
-    details = error.message;
+    error.message;
     stack = error.stack;
   }
 
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,4 @@
-import { Welcome } from "../welcome/welcome";
+
 import type { Route } from "./+types/home";
 
 export function meta({ }: Route.MetaArgs) {
@@ -9,5 +9,5 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <Welcome />;
+  return <div>Home page</div>
 }
